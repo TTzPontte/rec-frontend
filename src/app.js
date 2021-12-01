3d320e922012a82824a8d3ed7514faefcd2f5538
Rafael Perina <rafaelperina.98@gmail.com>
2021-12-01 17:59:54 -0300

    feat/tela-login/9613: configurado e criado interface para consumir servico amplify da aws

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ import { store } from './redux/store';
 import Boot from './redux/boot';
 import Routes from './router';
 import AppProvider from './AppProvider';
+import Amplify from '@aws-amplify/auth';
+import awsConfig from './config/amplify.config';
+
+Amplify.configure(awsConfig);
 
 const App = () => (
   <Provider store={store}>
--- src/containers/Pages/SignIn/SignIn.js
+++ src/containers/Pages/SignIn/SignIn.js
@@ -8,13 +8,13 @@ import {
   Logo,
   Form,
 } from "./SignIn.styles";
+import { logIn, logOut } from "@iso/lib/aws/amplify/index";
 import { ReactComponent as SvgLogoPontte } from "@iso/assets/logo-pontte.svg";
-
 import LogoGoogle from "@iso/assets/logo-google.svg";
 
 export default function SignIn() {
   const hasError = true;
-  const handleLogInFn = () => {};
+  const handleLogInFn = () => logIn(); // TODO: retirar
 
   return (
     <Container>
@@ -45,8 +45,7 @@ export default function SignIn() {
             <Message className="errorLogin">
               Parece que algo deu errado! Que tal tentarmos novamente?
             </Message>
-          ))()
-        }
+          ))()}
 
         <Message
           style={hasError ? { animation: "1s ease-out 0s 1 downMessage" } : {}}
