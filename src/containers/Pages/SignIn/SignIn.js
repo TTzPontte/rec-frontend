1c3598e0eb410c957e70b2870fd5e864eb9d1fb2
Rafael Perina <rafaelperina.98@gmail.com>
2021-12-01 17:37:53 -0300

    feat/tela-login/9613: criado parte visual da tela login

--- src/containers/Pages/SignIn/SignIn.js
+++ src/containers/Pages/SignIn/SignIn.js
@@ -1,84 +1,61 @@
-import React from 'react';
-import { Link, Redirect, useHistory, useLocation } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import Input from '@iso/components/uielements/input';
-import Button from '@iso/components/uielements/button';
-import IntlMessages from '@iso/components/utility/intlMessages';
-import authAction from '@iso/redux/auth/actions';
-import appAction from '@iso/redux/app/actions';
-import Auth0 from '../../Authentication/Auth0/Auth0';
-import SignInStyleWrapper from './SignIn.styles';
-import logo from '@iso/assets/images/logo/Logo-redeflex.svg';
+import React from "react";
+import {
+  Container,
+  Header,
+  Main,
+  BtnGoogle,
+  Message,
+  Logo,
+  Form,
+} from "./SignIn.styles";
+import { ReactComponent as SvgLogoPontte } from "@iso/assets/logo-pontte.svg";
 
-const { login } = authAction;
-const { clearMenu } = appAction;
+import LogoGoogle from "@iso/assets/logo-google.svg";
 
 export default function SignIn() {
-  let history = useHistory();
-  let location = useLocation();
-  const dispatch = useDispatch();
-  const isLoggedIn = useSelector((state) => state.Auth.idToken);
+  const hasError = true;
+  const handleLogInFn = () => {};
 
-  const [redirectToReferrer, setRedirectToReferrer] = React.useState(false);
-  React.useEffect(() => {
-    dispatch(login(false));
-    dispatch(clearMenu());
-    history.push('/dashboard');
-  });
-
-  function handleLogin(e, token = false) {
-    e.preventDefault();
-    if (token) {
-      dispatch(login(token));
-    } else {
-      dispatch(login());
-    }
-    dispatch(clearMenu());
-    history.push('/dashboard');
-  }
-  let { from } = location.state || { from: { pathname: '/dashboard' } };
-
-  if (redirectToReferrer) {
-    return <Redirect to={from} />;
-  }
   return (
-    <SignInStyleWrapper className="isoSignInPage">
-      <div className="isoLoginContentWrapper">
-        <div className="isoLoginContent">
-          <div className="isoLogoWrapper">
-          <Link to="/dashboard">
-              {/* <img alt="user" src={logo} style={{ height: '100px' }} /> */}
-            </Link>
-            <h1>Portal</h1>
-          </div>
-          <div className="isoSignInForm">
-            <form>
-              <div className="isoInputWrapper">
-                <Input
-                  size="large"
-                  placeholder="Username"
-                  autoComplete="true"
-                />
-              </div>
+    <Container>
+      <Header>
+        <Logo>
+          <SvgLogoPontte
+            style={{
+              width: "160px",
+              height: "36px",
+              color: "#5C3B6B",
+            }}
+          />
+        </Logo>
+      </Header>
+      <Main>
+        <Form>
+          <header>LOGIN - BASE CADASTRAL</header>
+          <main>
+            <BtnGoogle onClick={handleLogInFn}>
+              <img src={LogoGoogle} alt="Logo Google" />
+              <span> Login com o Google </span>
+            </BtnGoogle>
+          </main>
+        </Form>
 
-              <div className="isoInputWrapper">
-                <Input
-                  size="large"
-                  type="password"
-                  placeholder="Password"
-                  autoComplete="false"
-                />
-              </div>
+        {(() =>
+          hasError && (
+            <Message className="errorLogin">
+              Parece que algo deu errado! Que tal tentarmos novamente?
+            </Message>
+          ))()
+        }
 
-              <div className="isoInputWrapper isoCenterComponent">
-                <Button type="primary" onClick={handleLogin}>
-                  Entrar
-                </Button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </SignInStyleWrapper>
+        <Message
+          style={hasError ? { animation: "1s ease-out 0s 1 downMessage" } : {}}
+        >
+          Feito com
+          <span> ♥ </span>
+          pela Pontte
+        </Message>
+      </Main>
+    </Container>
   );
 }
