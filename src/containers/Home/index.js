ef3120ef7ea6d95e2e2368aca81ea3bc43f16bb8
Oliver Osti <oliverosti@gmail.com>
2021-12-01 15:08:54 -0300

    Home page
    Home page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,22 +1,62 @@
-import Button from '@iso/components/uielements/button';
+// import Button from '@iso/components/uielements/button';
+import { Button } from "antd";
+// import Input from '@iso/components/uielements/input';
+import { Input } from 'antd';
 import TableWrapper from '@iso/containers/Tables/AntTables/AntTables.styles';
 import { Col, Row } from 'antd';
+import { useState, useCallback } from 'react';
 import React from 'react';
 import './style.css';
+import {
+    Container,
+    Form,
+    Main,
+    InputDiv,
+    ButtonDiv,
+    IconBuscaDiv,
+  } from './styled-components';
+import { ReactComponent as IconCheck32x32 } from '../../assets/Icon-check-32x32.svg';  
 
-
+import Api from '../../api';
 
 export default function Home({ handleOperacao }) {
 
     const tableColumns = [
-
         {
-            title: 'Nome',
-            dataIndex: 'nome',
-            rowKey: 'nome',
+            title: 'Id Operação',
+            dataIndex: 'uuid',
+            rowKey: 'uuid',
             width: '5%',
             render: (text) => <span>{text}</span>,
         },
+        {
+            title: 'Origem',
+            dataIndex: 'origem',
+            rowKey: 'origem',
+            width: '5%',
+            render: (text) => <span>{text}</span>,
+        },        
+        {
+            title: 'Valor da Operação',
+            dataIndex: 'valorSolicitado',
+            rowKey: 'valorSolicitado',
+            width: '5%',
+            render: (text) => <span>{text}</span>,
+        },                
+        {
+            title: 'Tomador de Empréstimo',
+            dataIndex: 'tomador',
+            rowKey: 'tomador',
+            width: '5%',
+            render: (text) => <span>{text}</span>,
+        },                        
+        {
+            title: 'CPF Tomador',
+            dataIndex: 'cpfTomador',
+            rowKey: 'cpfTomador',
+            width: '5%',
+            render: (text) => <span>{text}</span>,
+        },                                
         {
             title: 'Ação',
             dataIndex: 'nome',
@@ -32,8 +72,6 @@ export default function Home({ handleOperacao }) {
                 </Button>
             ),
         },
-
-
     ];
 
     const operacoes = {
@@ -96,6 +134,24 @@ export default function Home({ handleOperacao }) {
         }
     };
 
+    const api = new Api();
+    const [textoBusca, setTextoBusca] = useState('');
+    const [processos, setProcessos] = useState([]);
+
+    function handleTextBuscaFn(evento: any) {
+        setTextoBusca(evento.target.value);
+      }
+      const handleTextBusca = useCallback(handleTextBuscaFn, []);
+    
+      async function handleSubmitFn(evento: any) {
+        evento.preventDefault();
+        await api.busca(
+          '/processo/search?term='.concat(textoBusca).concat('&page=0&size=100'),
+          setProcessos
+        );
+      }
+      const handleSubmit = useCallback(handleSubmitFn, [textoBusca]);    
+
     const handleTableChange = (e) => {
     };
 
@@ -109,26 +165,52 @@ export default function Home({ handleOperacao }) {
         marginBottom: '5px',
     };
 
-    const divStyle = {
-        border: '1px',
-        solid: '#000',
-    };
-
     const gutter = 16;
     return (
-        <Row style={rowStyle} gutter={gutter} justify="start">
-            <Col sm={18} md={20} xs={22} style={colStyle}>
-                <TableWrapper
-                    dataSource={operacoes.payload.content}
-                    columns={tableColumns}
-                    pagination={{
-                        total: operacoes.payload.totalElements,
-                        pageSize: operacoes.payload.size,
-                    }}
-                    style={{ marginTop: '20px', width: '100%' }}
-                    onChange={handleTableChange}
-                />
-            </Col>
-        </Row>
+        <Container>
+            <Main>
+                <Form>
+                    <div>
+                        <IconBuscaDiv>
+                            <IconCheck32x32 />
+                        </IconBuscaDiv>
+                        <InputDiv>
+                            <Input 
+                                name="texto-busca"
+                                bordered={false}
+                                className="home_input-busca"
+                                onChange={handleTextBusca}
+                            />
+                        </InputDiv>
+                        <ButtonDiv>
+                            <Button
+                                type="primary"
+                                className="home_botao-busca"
+                                onClick={handleSubmit}
+                            >
+                                Pesquisar
+                            </Button>
+                        </ButtonDiv>
+                    </div>
+                </Form>
+
+                <Row style={rowStyle} gutter={gutter} justify="start">
+                    <Col span={6}></Col>
+                    <Col span={12} style={colStyle}>
+                        <TableWrapper
+                            dataSource={processos}
+                            columns={tableColumns}
+                            pagination={{
+                                total: operacoes.payload.totalElements,
+                                pageSize: operacoes.payload.size,
+                            }}
+                            style={{ marginTop: '20px', width: '100%' }}
+                            onChange={handleTableChange}
+                        />
+                    </Col>
+                    <Col span={6}></Col>
+                </Row>
+            </Main>
+        </Container>        
     );
 }
