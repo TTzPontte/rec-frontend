8355ef2bfae56c36ae12ab4bb66b9132487d2603
Rafael Perina <rafaelperina.98@gmail.com>
2021-12-09 16:37:46 -0300

    feat/tela-login/9613: implementado rotina do refresh token pelo redux e axios

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -1,37 +1,29 @@
 export const ACTIONS = {
   VERIFY_USER_AUTHENTICATION: "VERIFY_USER_AUTHENTICATION",
+  REFRESH_TOKEN: "REFRESH_TOKEN",
   LOGIN_REQUEST: "LOGIN_REQUEST",
   LOGOUT: "LOGOUT",
   LOGIN_SUCCESS: "LOGIN_SUCCESS",
   LOGIN_ERROR: "LOGIN_ERROR",
-  RESET: "RESET"
+  RESET: "RESET",
 };
 
-/**
- * Ação disparada quando no momento do login
- */
 export const logInAct = () => ({
   type: ACTIONS.LOGIN_REQUEST,
 });
 
-/**
- * Ação de logout da sessão
- */
 export const logOutAct = () => ({
   type: ACTIONS.LOGOUT,
 });
 
-/**
- * Ação disparada para verificar sessão do usuário e captura-la
- */
 export const verifyUserAuthentication = () => ({
   type: ACTIONS.VERIFY_USER_AUTHENTICATION,
 });
 
-
-/**
- * Ação que dispara quando há  erro no login
- */
- export const logInErrorAct = () => ({
+export const logInErrorAct = () => ({
   type: ACTIONS.LOGIN_ERROR,
+});
+
+export const refreshTokenAct = () => ({
+  type: ACTIONS.REFRESH_TOKEN,
 });
