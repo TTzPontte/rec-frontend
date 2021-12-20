c2ee155fab06fbef55c08c4b0732d2b68c8132d1
Oliver Osti <oliverosti@gmail.com>
2021-12-20 17:49:13 -0300

    Ajustes

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -1,7 +1,19 @@
 export default class StringUtils {
 
+  static isNotNull(obj) {
+      let rt = true;
+      if (typeof obj === 'undefined') {
+          rt = false;
+      } else if (obj === null) {
+          rt = false;
+      } else if (obj === '') {
+          rt = false;
+      }
+      return rt;
+  }
+
     static truncateString(str, num) {
-        if (str.length > num) {
+        if (StringUtils.isNotNull(str) && str.length > num) {
           return str.slice(0, num) + "...";
         } else {
           return str;
