56e38ae635cb202c6b9b5a541f147c5bd00a5759
Rafael Perina <rafaelperina.98@gmail.com>
2022-01-21 16:27:34 -0300

    style: formatado identacao

--- src/api/index.js
+++ src/api/index.js
@@ -19,7 +19,9 @@ class Api {
   async salvarPessoaNoProcesso(dados) {
     let processo = null;
     try {
-      processo = await this.buscarProcessoByUuid('/processo/'.concat(dados.processo + '/0/1'));
+      processo = await this.buscarProcessoByUuid(
+        "/processo/".concat(dados.processo + "/0/1")
+      );
     } catch (e) {
       return e.message;
     }
@@ -31,23 +33,39 @@ class Api {
     let retorno = {};
     let pessoa = null;
     if (StringUtils.isNotNull(dados.pessoaTipo) && dados.pessoaTipo === "PF") {
-      pessoa = {nome: dados.nome, cpf: dados.cpf, pessoaTipo: 'PF'};
+      pessoa = { nome: dados.nome, cpf: dados.cpf, pessoaTipo: "PF" };
     }
     if (StringUtils.isNotNull(dados.pessoaTipo) && dados.pessoaTipo === "PJ") {
-      pessoa = {razaoSocial: dados.razaoSocial, cnpj: dados.cnpj, pessoaTipo: 'PJ'};
-    }  
+      pessoa = {
+        razaoSocial: dados.razaoSocial,
+        cnpj: dados.cnpj,
+        pessoaTipo: "PJ",
+      };
+    }
     if (StringUtils.isNotNull(pessoa)) {
       try {
-        let resposta = await this.service.post('/pessoa', pessoa);
+        let resposta = await this.service.post("/pessoa", pessoa);
         pessoa = resposta.data;
       } catch (e) {
         retorno = e.message;
-      }    
-    }  
-    if (StringUtils.isNotNull(pessoa) && StringUtils.isNotNull(pessoa.id) && StringUtils.isNotNull(processo) && StringUtils.isNotNull(processo.id)) {
-      let processoEnvolvidos = {tipo: dados.envolvimento, pessoa: {id: pessoa.id}, processo: {id: processo.id}};
+      }
+    }
+    if (
+      StringUtils.isNotNull(pessoa) &&
+      StringUtils.isNotNull(pessoa.id) &&
+      StringUtils.isNotNull(processo) &&
+      StringUtils.isNotNull(processo.id)
+    ) {
+      let processoEnvolvidos = {
+        tipo: dados.envolvimento,
+        pessoa: { id: pessoa.id },
+        processo: { id: processo.id },
+      };
       try {
-        let resposta = await this.service.post('/processo-envolvidos', processoEnvolvidos);
+        let resposta = await this.service.post(
+          "/processo-envolvidos",
+          processoEnvolvidos
+        );
         processoEnvolvidos = resposta.data;
         retorno = processoEnvolvidos;
       } catch (e) {
@@ -60,7 +78,7 @@ class Api {
   async salvarProcesso(url, formValues) {
     return await this.service.patch(url, formValues);
   }
-  
+
   async alterarProcesso(url, formValues) {
     return await this.service.patch(url, formValues);
   }
@@ -81,7 +99,7 @@ class Api {
     return await this.service.post(endpoint, payload);
   }
 
-  async salvar(endpoint, payload){
+  async salvar(endpoint, payload) {
     return await this.service.post(endpoint, payload);
   }
 
